406ae6c295234f96f18c8236e5dd59304b49c3b2
Launchpad <admin@rdpstudio.top>
2023-04-28 12:52:55 +0800

    Update require-tag-on-topic-creation.js

--- assets/javascripts/discourse/initializers/require-tag-on-topic-creation.js
+++ assets/javascripts/discourse/initializers/require-tag-on-topic-creation.js
@@ -1,6 +1,10 @@
 import I18n from "I18n";
-import { Promise } from "rsvp";
-import { withPluginApi } from "discourse/lib/plugin-api";
+import {
+  Promise
+} from "rsvp";
+import {
+  withPluginApi
+} from "discourse/lib/plugin-api";
 
 export default {
   name: "require-tag-on-topic-creation",
@@ -9,6 +13,49 @@ export default {
     const siteSettings = container.lookup("service:site-settings");
     if (siteSettings.discourse_global_filter_enabled) {
       withPluginApi("1.3.0", (api) => {
+        api.modifyClass("route:discovery", {
+          pluginId: "prefill-composer-tags",
+          actions: {
+            createTopic() {
+              try {
+                const hasDraft = this.currentUser?.has_topic_draft;
+                if (hasDraft) {
+                  this._super(...arguments);
+                  return;
+                } else {
+                  const controller = this.controllerFor("discovery/topics");
+                  const composerController = this.controllerFor("composer");
+                  const categoryId = controller.category?.id;
+                  const categorySlug = controller.category?.slug;
+                  try {
+                    var tag = router.currentRoute?.queryParams?.tag || "sq";
+                  } catch {
+                    var tag = "sq";
+                  }
+
+                  composerController
+                    .open({
+                      categoryId: categoryId,
+                      action: Composer.CREATE_TOPIC,
+                      draftKey: Composer.NEW_TOPIC_KEY
+                    })
+                    .then(() => {
+                      if (composerController.canEditTags && categoryId) {
+                        const composerModel = composerController.model;
+                        composerModel.set(
+                          "tags",
+                          makeArray([tag]).filter(Boolean)
+                        );
+                      }
+                    });
+                }
+              } catch {
+                this._super(...arguments);
+                return;
+              }
+            }
+          }
+        });
         api.composerBeforeSave(() => {
           return new Promise((resolve, reject) => {
             const composerModel = api.container.lookup(
@@ -29,7 +76,7 @@ export default {
 
             if (
               composerModel.tags.filter((tag) => globalFilters.includes(tag))
-                .length > 0
+              .length > 0
             ) {
               return resolve();
             } else {
